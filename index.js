7cb56167effd9880a35ed115c95c9979681320e6
Phan Thanh Trường <truong.phan@mservice.com.vn>
2023-06-25 22:52:09 +0700

    [update] db

--- index.js
+++ index.js
@@ -44,7 +44,24 @@
 // module.exports = app
 
 // index.js
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.czrhoe2.mongodb.net/nft?retryWrites=true&w=majority`
+    );
+
+    console.log("DB connected");
+  } catch (error) {
+    console.log(error);
+    console.log("loi roii!!!");
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 const app = express();
 const PORT = 4000;
